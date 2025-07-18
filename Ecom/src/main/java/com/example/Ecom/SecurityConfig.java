e3eb2faef7d834fdfcae89d076335a5e527c646d
Canushka <anushkachatterjee1104@gmail.com>
2025-07-18 09:57:41 +0530

    Added user registration & login with DTO, tested with Postman

--- Ecom/src/main/java/com/example/Ecom/SecurityConfig.java
+++ Ecom/src/main/java/com/example/Ecom/SecurityConfig.java
@@ -1,15 +1,21 @@
-package com.example.Ecom;
+package com.example.Ecom.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+        return http.build();
     }
 }
